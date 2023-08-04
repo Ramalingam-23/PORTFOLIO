67efd69cc52a3f88e9a55ce8aafd2c9c8dabe62b
Ramalingam-23 <110720984+Ramalingam-23@users.noreply.github.com>
2023-08-04 14:59:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+
+
+
+
+function sendMail(event){
+    console.log(document.getElementById("emailaddress").value);
+    event.preventDefault();
+    var params = {
+        fullname: document.getElementById("fullname").value,
+        emailaddress: document.getElementById("emailaddress").value,
+        mobilenumber: document.getElementById("mobilenumber").value,
+        emailsubject: document.getElementById("emailsubject").value,
+        message: document.getElementById("message").value
+    };
+console.log(document.getElementById("fullname").value)
+const serviceID="service_0xaf8im";
+const templateID="template_dwdoqy8";
+
+emailjs.send(serviceID,templateID,params)
+.then((res) =>{
+        document.getElementById("fullname").value = "";
+        document.getElementById("emailaddress").value = "";
+        document.getElementById("mobilenumber").value = "";
+        document.getElementById("emailsubject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+    }
+)
+.catch((err)=>console.log(err));
+
+}
+
+function GD(){
+    alert('Yet to Finish!');
+}
+
+function DM(){
+    alert('Yet to Finish!');
+}
+function WD(){
+    alert('Yet to Finish!');
+}
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Front End Developer','B.E,Computer Science Engineer','Designer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+
